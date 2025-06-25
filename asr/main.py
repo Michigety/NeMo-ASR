871fdee6bee64d08eaf9a58ddcb1b0e506e1b1a5
Michigety <bizanlil94@gmail.com>
2025-06-26 00:45:09 +0900

    Add comments to yaml files in asr/configs/

--- asr/main.py
+++ asr/main.py
@@ -3,7 +3,7 @@ from omegaconf import DictConfig, OmegaConf
 
 @hydra_runner(config_path="configs", config_name="config")
 def main(cfg: DictConfig) -> None:
-    print(OmegaConf.to_yaml(cfg))
+    print(OmegaConf.to_yaml(cfg, resolve=True))
 
 if __name__ == "__main__":
     main()
